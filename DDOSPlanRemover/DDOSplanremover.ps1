a06ab42cd0959a9bf1dbdbc5026764a04cb74c3e
itskarthiksrinivas <itskarthiksrinivas@gmail.com>
2018-12-29 21:19:18 +0530

    Add files via upload

---
+++ DDOSPlanRemover/DDOSplanremover.ps1
@@ -0,0 +1,35 @@
+﻿Connect-AzureRmAccount
+$SubID = Read-Host -Prompt 'Enter your SubID'
+Select-AzureRmSubscription -Subscriptionid $SubID
+$WarningPreference = 'SilentlyContinue'
+$rmvnets=Get-AzureRmVirtualNetwork 
+
+foreach ($vnet in $rmvnets) 
+    { 
+    $WarningPreference = 'SilentlyContinue'
+    $ps= Get-AzureRmVirtualNetwork `
+    -ResourceGroupName $vnet.ResourceGroupName `
+    -Name $vnet.Name    
+if($ps.DdosProtectionPlan){
+write-host $vnet.Name ("Vnet has DDOS enabled and Will list down the plan below. Please delete")
+$ps.DdosProtectionPlan
+$vnet.ResourceGroupName
+$vnet.DdosProtectionPlan = $null
+$vnet.EnableDdosProtection = $false
+$vnet | Set-AzureRmVirtualNetwork -asjob
+} 
+       
+ else {
+        write-host $vnet.Name ("Vnet has no DDOS plan enabled!")
+      }
+          }
+  
+$plans= Get-AzureRmDdosProtectionPlan
+foreach ($plan in $plans) 
+    { 
+    $WarningPreference = 'SilentlyContinue'
+    $dos= Get-AzureRmDdosProtectionPlan `
+    -ResourceGroupName $plan.ResourceGroupName `
+    -Name $plan.Name 
+    Remove-AzureRmDdosProtectionPlan -ResourceGroupName $dos.ResourceGroupName -Name $dos.Name
+    }
