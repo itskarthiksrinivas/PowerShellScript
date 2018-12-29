60f048b43f1f7a53a45498d6a2dd8da57b8e3143
itskarthiksrinivas <itskarthiksrinivas@gmail.com>
2018-12-29 18:38:14 +0530

    Add files via upload
    This Script will turn OOF all your VMs in your Subscription.

---
+++ PStoTurnOFFVM.ps1
@@ -0,0 +1,27 @@
+﻿//$ps=Get-AzureRmVM `
+    -ResourceGroupName "AzureWAN" `
+    -Name "VM1" `
+    -Status | Select {$_.Statuses[1].Code -Match "PowerState/running"}
+
+
+
+    while(1) {
+
+    $ps=Get-AzureRmVM `
+    -ResourceGroupName "AzureWAN" `
+    -Name "VM1" `
+    -Status |Select {$_.Statuses[1].Code -Match "PowerState/running"}
+    if($ps -match "True")
+        {
+write-host("VM is Turned On! OOPS let me turn it OFF for you")
+		
+Stop-AzureRmVM `
+   -ResourceGroupName "AzureWAN" `
+   -Name "VM1" -Force 
+		}
+ else {
+        write-host("VM is Turned OFF already")
+       }
+
+		
+}
