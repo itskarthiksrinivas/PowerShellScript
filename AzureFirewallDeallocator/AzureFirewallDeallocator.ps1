267db49d747ab85cc5745c142120952146784a97
itskarthiksrinivas <itskarthiksrinivas@gmail.com>
2018-12-29 21:21:57 +0530

    Add files via upload

---
+++ AzureFirewallDeallocator/AzureFirewallDeallocator.ps1
@@ -0,0 +1,17 @@
+﻿Connect-AzureRmAccount
+$SubID = Read-Host -Prompt 'Enter your SubID'
+Select-AzureRmSubscription -Subscriptionid $SubID
+$WarningPreference = 'SilentlyContinue'
+$AFs=Get-AzureRmFirewall 
+
+foreach ($AF in $AFs) 
+    { 
+    $ps= Get-AzureRmFirewall `
+    -ResourceGroupName $AF.ResourceGroupName `
+    -Name $AF.Name
+    echo $ps
+
+    $AF.Deallocate() 
+    Set-AzureRmFirewall -AzureFirewall $AF
+
+    } 
