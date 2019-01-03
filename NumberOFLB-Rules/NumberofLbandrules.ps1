4da13206a9d2fc784510fbe894af4f324d4b74a0
itskarthiksrinivas <itskarthiksrinivas@gmail.com>
2019-01-03 08:10:16 +0530

    Add files via upload

---
+++ NumberOFLB-Rules/NumberofLbandrules.ps1
@@ -0,0 +1,16 @@
+﻿Connect-AzureRmAccount
+$SubID = Read-Host -Prompt 'Enter your SubID'
+Select-AzureRmSubscription -Subscriptionid $SubID
+$a=Get-AzureRmLoadBalancer
+Write-host "Number of Lb in your subscription is " $a.count -ForegroundColor Cyan -BackgroundColor Black 
+
+foreach($b in $a) {
+
+$lb=get-azurermloadbalancer -ResourceGroupName $b.ResourceGroupName -Name $b.Name 
+$lb
+$rules=Get-AzureRmLoadBalancerRuleConfig -LoadBalancer $lb 
+$natrules=Get-AzureRmLoadBalancerInboundNatRuleConfig -LoadBalancer $lb
+$total= $rules.count + $natrules.count 
+Write-host "Number of rules in " $lb.Name "is "$total -ForegroundColor Cyan -BackgroundColor Black
+
+}
